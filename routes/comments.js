55ea88866d75539cec6c844aa0f53d06cb6912f8
Rafael Carvalho <rfroesmonteiroc1649@conestogac.on.ca>
2020-08-05 18:07:48 -0400

    added validation to add comment

--- routes/comments.js
+++ routes/comments.js
@@ -2,16 +2,26 @@ const express = require("express");
 const router = express.Router();
 const Comment = require("../models/comment");
 const User = require("../models/user");
+const Cart = require("../models/cart");
 const db = require("../configs/database");
 
 router.post('/', async (req, res) => {
 	let userId = req.session.userId || null;
+	let hasCart = null;
 	if(userId){
-		await newComment(userId, req.body);
-    	res.status(200).json({
-      		message: 'Commented successfully',
-      		status: 'Success'
-    	});
+		hasCart = await canComment(userId, req.body.productId);
+		if(hasCart){
+			await newComment(userId, req.body);
+    		res.status(200).json({
+      			message: 'Commented successfully',
+      			status: 'Success'
+    		});
+		}else{
+			res.status(400).json({
+      			message: 'You cant comment if you havent bought the product',
+      			status: 'Fail'
+    		});
+		}
 	}else{
 		res.status(400).json({
       		message: 'Log in to comment',
@@ -19,6 +29,11 @@ router.post('/', async (req, res) => {
     	});
 	}
 });
+
+async function canComment(userId, productId){
+	let cart = Cart.findOne({userId: userId, status: 3, "products.id": productId});
+	return cart;
+}
 
 async function newComment(userId, body) {
   let comment = new Comment();
