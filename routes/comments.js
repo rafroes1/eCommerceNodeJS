f5a8eaa52ee908aff77ec764ee63d365a0e4b3ab
Rafael Carvalho <rfroesmonteiroc1649@conestogac.on.ca>
2020-08-05 14:20:47 -0400

    add new comment working as it should.

--- routes/comments.js
+++ routes/comments.js
@@ -1,5 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const Comment = require("../models/comment");
+const db = require("../configs/database");
+
+router.post('/', async (req, res) => {
+	let userId = req.session.userId || null;
+	if(userId){
+		await newComment(userId, req.body);
+    	res.status(200).json({
+      		message: 'Commented successfully',
+      		status: 'Success'
+    	});
+	}else{
+		res.status(400).json({
+      		message: 'Log in to comment',
+      		status: 'Fail'
+    	});
+	}
+});
+
+async function newComment(userId, body) {
+  let comment = new Comment();
+
+  comment.userId = userId;
+  comment.productId = body.productId;
+  comment.rating = body.rating;
+  comment.content = body.content;
+
+  await comment.save();
+}
 
 module.exports = router;
