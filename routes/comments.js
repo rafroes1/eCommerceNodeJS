dc9f9def60b8ef0c7c85299f6b638dec2106977d
Rafael Carvalho <rfroesmonteiroc1649@conestogac.on.ca>
2020-08-05 16:24:04 -0400

    get all comments and all comments by productId properly working.

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Comment = require("../models/comment");
+const User = require("../models/user");
 const db = require("../configs/database");
 
 router.post('/', async (req, res) => {
@@ -30,4 +31,25 @@ async function newComment(userId, body) {
   await comment.save();
 }
 
+router.get('/', async (req, res) => {
+	let comments = await Comment.find();
+	res.status(200).json(comments);
+});
+
+router.get('/:productId', async (req, res) => {
+	let output = [];
+
+	let comments = await Comment.find({ productId: req.params.productId }).select('userId content rating updated_at').cursor()
+		.eachAsync(async function(doc, i){
+			var obj = doc.toObject();
+			let userFullname = await User.findById(doc.userId).select('fullname');
+			obj.fullname = userFullname.fullname;
+			output.push(obj);
+			console.log(obj);
+		});
+
+	res.status(200).json(output);
+});
+
 module.exports = router;
+
