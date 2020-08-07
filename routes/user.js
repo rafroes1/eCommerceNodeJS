b33e883b1692a9e8c674036046d14985742a96ea
Serhii Putilov <sputilov0113@conestogac.on.ca>
2020-08-07 00:21:14 -0400

    Account created and updated

--- routes/user.js
+++ routes/user.js
@@ -16,6 +16,60 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/create-account', async (req, res) =>{
+  let email = req.body.email;
+  let password = req.body.password;
+  let fullname = req.body.fullname;
+  let phone = req.body.phone;
+  let address = req.body.email;
+
+  if( email !== undefined && password !== undefined && fullname !== undefined && phone !== undefined && address !== undefined){
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync(password, salt);
+    let user = new User();
+    user.email = email;
+    user.password = hash;
+    user.fullname = fullname;
+    user.phone = phone;
+    user.address = address;
+    await user.save();
+    res.status(200).json({
+      message: 'Account was successfully created.'
+    }); 
+  }else {
+      res.status(400).json({
+        message: 'Missing parameters.'
+      });
+  }
+});
+
+router.post('/update-account', async (req, res) =>{
+  let userId = req.session.userId;
+  if(userId){
+    let user = await User.findById(userId);
+    if(req.body.email !== undefined){
+      user.email = req.body.email;
+    }
+    if(req.body.fullname !== undefined){
+      user.fullname = req.body.fullname;
+    }
+    if(req.body.phone !== undefined){
+      user.phone = req.body.phone;
+    }
+    if(req.body.address !== undefined){
+      user.address = req.body.address;
+    }
+        await user.save();
+        res.status(200).json({
+          message: 'Account was successfully updated.'
+        });
+  }else {
+    res.status(401).json({
+      message: 'Please login into your account.'
+    });
+  }
+});
+
 router.put('/change-password', async (req, res) => {
   let userId = req.session.userId;
   if(userId) {
