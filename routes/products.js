1e6626af2c5da4c4d1233b670b54f7226c1cadb7
Eric <ericen01@gmail.com>
2020-08-08 12:27:26 -0400

    added products.js

--- routes/products.js
+++ routes/products.js
@@ -2,4 +2,28 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/product");
 
+
+    // get all products
+    
+    router.get('/', async (req, res) => {
+  
+    await Product.find({}, function(err, result){
+        console.log(result);
+        res.status(200).json(result);
+    });
+  });
+
+    
+  // get products by typing its product name
+
+  router.get('/:productId', async (req, res) => {
+
+    const productName = req.params['productId'];
+    console.log(productName);
+     await Product.findOne({ name: productName }, function(err, result){
+        console.log(result);
+        res.status(200).json(result);
+    });
+  });
+
 module.exports = router;
